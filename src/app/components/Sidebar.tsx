fcc4b711686d0cdb1b166ee001d23a161b747c19
sayo-2123 <classa1990@gmail.com>
2024-05-15 11:22:06 +0900

    room新規作成の実装

--- src/app/components/Sidebar.tsx
+++ src/app/components/Sidebar.tsx
@@ -1,6 +1,6 @@
 'use client'
 
-import { collection, onSnapshot, orderBy, query, Timestamp, where } from 'firebase/firestore';
+import { addDoc, collection, onSnapshot, orderBy, query, serverTimestamp, Timestamp, where } from 'firebase/firestore';
 import React, { useEffect, useState } from 'react'
 import { BiLogOut } from 'react-icons/bi'
 import { db } from '../firebase';
@@ -46,11 +46,24 @@ function Sidebar() {
     setSelectedRoom(roomId)
   };
 
+  const addNewRoom = async () => {
+    const roomName = prompt("ルーム名を入力してください")
+    if (roomName) {
+      const newRoomRef = collection(db, "rooms")
+      await addDoc(newRoomRef, {
+        name: roomName,
+        userId: userId,
+        createdAt: serverTimestamp(),
+      })
+    }
+  }
+
 
   return (
     <div className=' bg-custom-blue h-full overflow-y-auto px-5 flex flex-col'>
       <div className='flex-grow'>
-        <div className='cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150'>
+        <div onClick={addNewRoom}
+          className='cursor-pointer flex justify-evenly items-center border mt-2 rounded-md hover:bg-blue-800 duration-150'>
           <span className='text-white p-4 text-2xl'>＋</span>
           <h1 className='text-white text-xl font-semibold p-4'>New Chat</h1>
         </div>
@@ -63,16 +76,6 @@ function Sidebar() {
               {room.name}
             </li>
           ))}
-
-          {/* <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
-            Room2
-          </li>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
-            Room3
-          </li>
-          <li className='cursor-pointer border-b p-4 text-slate-100 hover:bg-slate-700 duration-150'>
-            Room4
-          </li> */}
 
         </ul>
       </div>
